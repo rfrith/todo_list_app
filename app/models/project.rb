f78ae59611423613e7d9db2f6d97456e892faa18
Ryan Frith <ryan.frith@icloud.com>
2015-05-26 19:21:44 -0400

    Added fix for bug "Projects with the same title should not be allowed"

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
   validates :title, :presence => true
+  validates_uniqueness_of :title, :message => ' has already been used.  Please choose a different value.'
   has_many :items, :dependent => :destroy
 end
