4a2e6c72f113378011c40b7d5f9d562758778519
Ryan Frith <ryan.frith@icloud.com>
2015-05-26 19:35:15 -0400

    Added spec for "Projects with the same title should not be allowed""

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -20,5 +20,15 @@ RSpec.describe Project, :type => :model do
       expect(subject).to_not be_valid
       expect(subject.errors.keys).to eq [:title]
     end
+
+    it "requires title be unique across all projects" do
+      subject.save
+
+      duplicate_action = Project.new(params)
+
+      expect(duplicate_action).to_not be_valid
+      expect(duplicate_action.errors.keys).to include :title
+    end
+
   end
 end
