6725931708185eb321d587755a806be16265109d
Ryan Frith <ryan.frith@icloud.com>
2015-05-26 22:37:25 -0400

    Database seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+p1 = Project.create(:title => "Project 1")
+p2 = Project.create(:title => "Project 2", :deleted => true)
+p3 = Project.create(:title => "Project 3")
+
+Item.create(:action => "Item 1", :project => p1)
+Item.create(:action => "Item 2", :project => p1)
+Item.create(:action => "Item 3", :project => p1)
+
+Item.create(:action => "Item 1", :project => p2, :deleted => true)
+Item.create(:action => "Item 2", :project => p2, :deleted => true)
+Item.create(:action => "Item 3", :project => p2)
